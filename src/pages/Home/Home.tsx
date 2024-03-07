baf4a08a9db9ff0eec7ad5e0458b3941285904bc
park-moen <pmj6516@naver.com>
2024-03-07 21:27:57 +0900

    refactor: pages 폴더 추가
    - App.tsx에 종속된 컴포넌트 pages 폴더로 이동
    - 불필요한 파일 App.css 삭제

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header.tsx";
+
+import "@/pages/Home/home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main className="main"></main>
+    </div>
+  );
+};
+
+export default Home;
